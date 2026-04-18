9e7dc601831ef047c8be1fa093303bbd677d3f22
Lain-coder <Lain2634222196@outlook.com>
2026-04-18 20:39:50 +0800

    for增强

--- src/com/collection/CollectionFor.java
+++ src/com/collection/CollectionFor.java
@@ -14,6 +14,34 @@ public class CollectionFor {
         //1.增强for的使用,在Collection集合
         //2.增强for，底层仍然是迭代器
         //3.增强for可以理解为简化版的迭代器遍历
+        /*它到底解决了什么？
+✅ 1. 不再关心索引
+        for (int num : arr)
+
+👉 直接拿元素，不需要 i
+
+✅ 2. 更安全（不会越界）
+
+        你不会再写出：
+
+        i <= arr.length
+
+        这种错误
+
+✅ 3. 统一遍历方式
+
+        不管是：
+
+        int[] arr
+        List<String> list
+        Set<Integer> set
+
+        都可以用：
+
+        for (元素 : 集合)
+
+👉 统一语法
+*/
         for (Object book : collection) {
             System.out.println("book = " + book);
         }
