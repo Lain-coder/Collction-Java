32776e5fb58bcc79a36bac6876a80453b3cf9edd
Lain-coder <Lain2634222196@outlook.com>
2026-04-23 20:50:37 +0800

    List操作方法使用

---
+++ src/com/list/ListMethod.java
@@ -0,0 +1,18 @@
+package com.list;
+
+public class ListMethod {
+    /* List接口提供了一系列方法来操作集合中的元素：
+
+    添加元素：add(E e)、addAll(Collection<? extends E> c)
+
+    访问元素：get(int index)、set(int index, E element)
+
+    删除元素：remove(Object o)、remove(int index)
+
+    搜索：indexOf(Object o)、lastIndexOf(Object o)
+
+    迭代：listIterator()、listIterator(int index)
+
+    范围操作：subList(int fromIndex, int toIndex)
+    */
+}
