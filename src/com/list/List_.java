9a603283d26f1fcbb5facbee2609455a7e5f10c8
Lain-coder <Lain2634222196@outlook.com>
2026-04-23 16:31:11 +0800

    List使用

--- src/com/list/List_.java
+++ src/com/list/List_.java
@@ -12,9 +12,16 @@ public class List_ {
         list.add("herry");
         list.add("lain");
 
-        System.out.println(list);
+        System.out.println("list = "+ list);
+        list.lastIndexOf("jack");
         //按索引查找
         System.out.println(list.get(1));
+
+        list.remove(1);
+        System.out.println("list = " + list);
+
+        list.subList(2,3).clear();//左闭右开区间范围
+        System.out.println("list = " + list);
     }
 
 }
